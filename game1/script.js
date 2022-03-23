d4e717215f516c7674963adf8a4f6a17f5c64e32
Andrey Runov <elefant86@bk.ru>
2022-03-23 10:19:08 +0300

    Game

--- game1/script.js
+++ game1/script.js
@@ -1,10 +1,30 @@
-
+let previousRand = 0;
 function randomInt() {
   let min = 0;
   let max = 8;
+  console.log(previousRand);
   let rand = Math.floor(min + Math.random() * (max + 1 - min));
   console.log(rand);
-  return rand;
+  
+  if (previousRand !== rand) {
+/*     console.log('Тут все норм!');
+    console.log(`Предыдущий рандом: ${previousRand}`);
+    console.log(`Новый рандом: ${rand}`); */
+    previousRand = rand;
+    return rand;
+  } else if (previousRand === rand && rand <= 8 && rand >= 2) {
+/*     console.log('Тут делаем -1');
+    console.log(`Предыдущий рандом: ${previousRand}`);
+    console.log(`Новый рандом: ${rand}`); */
+    previousRand = rand - 1;
+    return rand - 1;
+  } else if (previousRand === rand && rand === 1 || rand === 0) {
+/*     console.log('Тут делаем +2');
+    console.log(`Предыдущий рандом: ${previousRand}`);
+    console.log(`Новый рандом: ${rand}`); */
+    previousRand = rand + 2;
+    return rand + 2;
+  }
 }
 
 let doc = document;
@@ -13,17 +33,18 @@ console.log(butterflyList);
 butterflyList[randomInt()].style.cssText = `
   display: inherit;
 `;
-console.log(butterflyList);
+
 let net = document.querySelectorAll('.net');
 console.log(net);
 let p = document.getElementsByTagName("p")[0];
 let target;
 let count = 0;
 doc.addEventListener('click', (event) => {
-  console.log(event.target);
+   console.log(event.target);
   for (let i = 0; i < butterflyList.length; i++) {
     if (event.target === butterflyList[i]) {
       target = event.target;
+      console.log(target);
       target.style.cssText = `
       display: none;
       `;
@@ -33,23 +54,18 @@ doc.addEventListener('click', (event) => {
           display: none;
           `;
       }
-      /* net.style.cssText = `
-      display: none;
-      `; */
       net[i].style.cssText = `
       display: inherit;
       `;
       p.innerText = `Поймано бабочек: ${count}`;
-    }
-  }
-  
 
-  setTimeout(function(){
-  butterflyList[randomInt()].style.cssText = `
-  display: inherit;
-  `; 
-  },2000); 
-  
+      setTimeout(function(){
+      butterflyList[randomInt()].style.cssText = `
+      display: inherit;
+      `; 
+      },2000); 
+    }
+  }  
 })
 
 
